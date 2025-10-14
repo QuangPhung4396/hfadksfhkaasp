5ffbba8ec4f5fd859c8e5b5e5fc5ea5dd05f5260
quangmacmini <>
2025-10-14 23:38:20 +0700

    no message

---
+++ QRoot.xcframework/ios-arm64/QRoot.framework/Headers/QRoot.h
@@ -0,0 +1,18 @@
+//
+//  QRoot.h
+//  QRoot
+//
+//  Created by quang on 14/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QRoot.
+FOUNDATION_EXPORT double QRootVersionNumber;
+
+//! Project version string for QRoot.
+FOUNDATION_EXPORT const unsigned char QRootVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRoot/PublicHeader.h>
+
+
